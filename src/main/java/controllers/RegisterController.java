755f5b826e226fc70add2191e139c11ea2bff534
Max Shannon <Maxshannon2016@hotmail.com>
2018-03-08 14:20:27 +0000

    loadLoginView method added in register controller

--- src/main/java/controllers/RegisterController.java
+++ src/main/java/controllers/RegisterController.java
@@ -1,11 +1,18 @@
 package controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 
 public class RegisterController {
 
+    @FXML
+    private AnchorPane registerStage;
     @FXML
     private TextField firstNameText;
     @FXML
@@ -22,7 +29,26 @@ public class RegisterController {
     @FXML
     private void loadLoginView(){
 
+        try{
 
+            Stage registerStage = new Stage();
+
+            Parent root = FXMLLoader.load(getClass().getResource("/view/login.fxml"));
+            registerStage.setTitle("Login");
+            registerStage.setScene(new Scene(root, 900, 500));
+            registerStage.show();
+
+            closeStage();
+
+        }catch (Exception e){
+
+            e.printStackTrace();
+        }
+    }
+
+    private void closeStage(){
+        Stage stage =(Stage) registerStage.getScene().getWindow();
+        stage.close();
     }
 
 }
