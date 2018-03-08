6ca59adcf0d04b3f3aa31f2a0d88315f650bdf3e
Max Shannon <Maxshannon2016@hotmail.com>
2018-03-08 14:16:17 +0000

    Registration view fields binded

---
+++ src/main/java/controllers/LoginController.java
@@ -0,0 +1,45 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class LoginController {
+
+    @FXML
+    private AnchorPane loginStage;
+    @FXML
+    private TextField userNameField;
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private void loadRegisterView(){
+
+        try{
+
+            Stage registerStage = new Stage();
+
+            Parent root = FXMLLoader.load(getClass().getResource("/view/registerview.fxml"));
+            registerStage.setTitle("Register here");
+            registerStage.setScene(new Scene(root, 900, 500));
+            registerStage.show();
+
+            closeStage();
+
+        }catch (Exception e){
+
+            e.printStackTrace();
+        }
+    }
+
+    private void closeStage(){
+        Stage stage =(Stage) loginStage.getScene().getWindow();
+        stage.close();
+    }
+}
---
+++ src/main/java/controllers/RegisterController.java
@@ -0,0 +1,28 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+
+public class RegisterController {
+
+    @FXML
+    private TextField firstNameText;
+    @FXML
+    private TextField secondNameText;
+    @FXML
+    private TextField emailText;
+    @FXML
+    private TextField userNameText;
+    @FXML
+    private TextField passwordOne;
+    @FXML
+    private TextField passwordTwo;
+
+    @FXML
+    private void loadLoginView(){
+
+
+    }
+
+}
