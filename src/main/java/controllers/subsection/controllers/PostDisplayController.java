9b0e6ff0c11c1288b7e623996901d271edfc8b4e
Max Shannon <Maxshannon2016@hotmail.com>
2018-03-26 21:28:03 +0000

    Display post view displaying generated post cards and styled

--- src/main/java/controllers/subsection/controllers/PostDisplayController.java
+++ src/main/java/controllers/subsection/controllers/PostDisplayController.java
@@ -1,7 +1,10 @@
 package controllers.subsection.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
 
@@ -13,13 +16,32 @@ public class PostDisplayController implements Initializable {
     public static String game;
 
     @FXML
-    private Text setTextLabel;
+    private VBox postsVbox;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        //setTextLabel.setText(game);
-        System.out.println(game);
+        display2Posts();
+        display2Posts();
+        display2Posts();
+        display2Posts();
+        display2Posts();
+        display2Posts();
+
+        //display2Posts();
     }
+
+     private void display2Posts(){
+
+        try{
+            AnchorPane node = FXMLLoader.load(getClass().getResource("/view/post_view.fxml"));
+            postsVbox.getChildren().add(node);
+
+        }catch (Exception e){
+
+            e.printStackTrace();
+        }
+
+     }
 
 }
