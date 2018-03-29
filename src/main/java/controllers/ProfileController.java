2d7423f84aa5d3f6e27f1db809b96668e87e9667
Max Shannon <Maxshannon2016@hotmail.com>
2018-03-29 12:12:46 +0100

    Profile - profile view added

--- src/main/java/controllers/ProfileController.java
+++ src/main/java/controllers/ProfileController.java
@@ -1,4 +1,35 @@
 package controllers;
 
-public class ProfileController {
+import functionality.DatabaseInteractionService;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
+import models.Post;
+
+import java.net.URL;
+import java.util.Collections;
+import java.util.ResourceBundle;
+
+public class ProfileController implements Initializable {
+
+    @FXML
+    private Pane profileBlueBar;
+    @FXML
+    private Pane postsBlueBar;
+    @FXML
+    private Pane groupsBlueBar;
+    @FXML
+    private Pane matchmakingBlueBar;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        //make other 3 bars invis
+        //load the profile view as default
+        postsBlueBar.setVisible(false);
+        groupsBlueBar.setVisible(false);
+        matchmakingBlueBar.setVisible(false);
+    }
+
+
 }
