d821460236f73d5135a6aedae945576e515139c6
Max Shannon <Maxshannon2016@hotmail.com>
2018-04-13 14:40:08 +0100

    group view controller added

---
+++ src/main/java/controllers/GroupMainViewController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class GroupMainViewController {
+
+    @FXML private Button createGroupBtn;
+
+    @FXML
+    private void openCreateViewView(){
+
+
+    }
+}
