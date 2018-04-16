2bf284b1a30ffe4b005ae92346baa8c15bc5ad84
Max Shannon <Maxshannon2016@hotmail.com>
2018-04-16 20:57:04 +0100

    Group information view added and unique group member view added

---
+++ src/main/java/controllers/dynamicviewcontrollers/UniqueGroupMemberController.java
@@ -0,0 +1,10 @@
+package controllers.dynamicviewcontrollers;
+
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+public class UniqueGroupMemberController {
+
+    public Text username;
+    public Rectangle onlineRect;
+}
