324c1d87521f4508be89c95ee565b4ff4d0f83ec
Max Shannon <Maxshannon2016@hotmail.com>
2018-04-16 14:06:17 +0100

    Group dynamically created views added and controller

---
+++ src/main/java/controllers/dynamicviewcontrollers/UniqueGroupController.java
@@ -0,0 +1,14 @@
+package controllers.dynamicviewcontrollers;
+
+import javafx.scene.text.Text;
+
+public class UniqueGroupController {
+
+    public Text groupName;
+    public Text gamePlayed;
+    public Text comsChannel;
+    public Text activityLevel;
+    public Text groupSize;
+
+
+}
--- src/main/java/controllers/innerviewcontrollers/GroupMainViewController.java
+++ src/main/java/controllers/innerviewcontrollers/GroupMainViewController.java
@@ -2,16 +2,20 @@ package controllers.innerviewcontrollers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class GroupMainViewController {
+public class GroupMainViewController implements Initializable {
 
     @FXML private AnchorPane injectablePane;
     @FXML private Button createGroupBtn;
-
+    @FXML private VBox groupsVbox;
 
     @FXML
     private void openCreateViewView() throws IOException{
@@ -19,4 +23,10 @@ public class GroupMainViewController {
         AnchorPane createGroupPane = FXMLLoader.load(getClass().getResource("/view/innerviews/creategroupview.fxml"));
         injectablePane.getChildren().setAll(createGroupPane);
     }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
 }
