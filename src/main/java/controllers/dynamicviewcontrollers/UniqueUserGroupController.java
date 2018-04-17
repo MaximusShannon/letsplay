7f852cc8af00328789da1efbd2f9191fa57fdd76
Max Shannon <Maxshannon2016@hotmail.com>
2018-04-17 14:20:58 +0100

    UniqueUserGroupController added

---
+++ src/main/java/controllers/dynamicviewcontrollers/UniqueUserGroupController.java
@@ -0,0 +1,20 @@
+package controllers.dynamicviewcontrollers;
+
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class UniqueUserGroupController {
+
+    public Text groupName;
+    public Text groupGame;
+    public Text groupVisibility;
+    public Text memberStatus;
+    public Text adminText;
+    public Text leaveText;
+    public ImageView adminImage;
+    public ImageView leaveImage;
+    public Button adminButton;
+    public Button leaveButton;
+
+}
