156a9ae45268b69c9ea1ffbbff65012590e0dec5
Max Shannon <Maxshannon2016@hotmail.com>
2018-04-21 13:55:06 +0100

    MatchedGroup dynamic view and controller added

---
+++ src/main/java/controllers/dynamicviewcontrollers/UniqueMatchedGroupController.java
@@ -0,0 +1,15 @@
+package controllers.dynamicviewcontrollers;
+
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class UniqueMatchedGroupController {
+
+    public Text groupName;
+    public Text gamePlaying;
+    public Text languageSpoken;
+    public Button gotoGroup;
+    public ImageView groupImage;
+
+}
